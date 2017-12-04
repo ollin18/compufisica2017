3abe4c44c3f9d19c55d69c44e49fc34cbb0d57b5
Joshua Briones <JoshuaBriones@ciencias.unam.mx>
2017-12-04 02:37:10 -0600

    Subí Practica comentada

--- Joshua/Tareas_Practicas/Practica1_Briones_Andrade_Joshua.sh
+++ Joshua/Tareas_Practicas/Practica1_Briones_Andrade_Joshua.sh
@@ -12,20 +12,23 @@ mkdir -p usuarios/python/scripts
 cd reinstalacion
 touch guia notas secuencia.data paquetes.pkg imagen.iso
 
+# Para poder pararnos en la carpeta padre, tenemos que retroceder dos veces
 cd..
 cd..
 
-#3)
+#3) Desplegar lista con todo lo que se encuentre (incluido archuvos secretos)
+# en el directorio padre con características y fecha de modificación
 ls -latR
 
-#4)
-cd respaldo_sistema
+#4) Copiar de forma todos los archivos y directorios del siguiente 
+#directorio padre
+cp -ar /bin programas respaldo_sistema
 
-#5)
+#5) Copiar de forma interactiv todos los archivos que comiencen 
+#con la letra x del sig directorio padre y que me informe el sistema 
 cp -iv /etc/X11/x* configuracion/X11
 
-#6)
-#Suponiendo que ya esta en modo super usuario "sudo"
+#6)Suponiendo que ya esta en modo super usuario "sudo"
 rm -r /etc/apache2 /var/www
 mv /etc/firefox-3.5/profile/bookmarks.html /etc/firefox-3.5/profile/favoritos.resp
 rm etc/*conf
