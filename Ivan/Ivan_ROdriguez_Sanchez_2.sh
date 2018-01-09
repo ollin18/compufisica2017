a410515f0024e18292f7ca867e13d33bac5e90c3
Ivan D'Pleur <33644062+godredeem@users.noreply.github.com>
2018-01-09 10:35:06 -0600

    Teras Practicas

---
+++ Ivan/Ivan_ROdriguez_Sanchez_2.sh
@@ -0,0 +1,26 @@
+touch linux.etc
+
+grep -r -l -i daemon /etc/* > linux.etc 
+#hacemos un filtrados de los archivos dados
+grep -r -l -i linux /etc/*.conf >> linux.etc
+
+cat /etc/passwd | less
+grep -e home | less
+
+find /usr -perm 777
+
+find /var -name *.tar
+
+touch MisProcesos.prs
+
+ps -fea >> MisProcesos.prs
+
+chmod 711 linux.etc
+
+chmod 740 MisProcesos.prs 
+
+touch todos.prm | chmod u+rwx,g-wx,o+rwx todos.prm
+
+touch grupo.prm | chmod 570 grupo.prm
+
+touch usuario.prm | chmod 755 usuario.prm
