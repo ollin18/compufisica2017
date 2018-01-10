58ea119312d88f0ec9a266c6e37cd466f4cff40e
vastialine2 <33643970+vastialine2@users.noreply.github.com>
2018-01-10 02:48:12 -0600

    mejorado

--- VastiHernandez/TareaPractica3_Hernández_Vargas_VastiAline.sh
+++ VastiHernandez/TareaPractica3_Hernández_Vargas_VastiAline.sh
@@ -3,9 +3,7 @@ touch bitacora.conx
 w > bitacora.conx
 cat bitacora.conx | less
 ifconfig
-echo se encuentra en el primer parrafo y  es el primer numero que tiene cua$
 touch red.conf
-#falta el punto 3
-mkdir PRACTICA_3
-tar -cvf PRACTICA_3_VELASCO_SANTIAGO_ALBERT_JAVIER.tar PRACTICA_3
-gzip -9 PRACTICA_3_VELASCO_SANTIAGO_ALBERT_JAVIER.tar
+mkdir Practica3
+tar -cvf Practica3_Hernandez_Vargas_VastiAline.tar Practica_3
+gzip -9 Practica3_Hernandez_Vargas_VastiAline.tar
