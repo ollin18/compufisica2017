cd6ab8e0fcddac2e017592e766d08702bdcaa34c
salmis19 <33644008+salmis19@users.noreply.github.com>
2018-01-10 10:28:15 -0600

    Add files via upload

---
+++ Salma/practica3_Garcia_ortiz_salma.sh
@@ -0,0 +1,11 @@
+ssh est7@132.248129.8.
+touch bitacora.conx
+w > bitacora.conx
+cat bitacora.conx | less
+ifconfig
+echo se encuentra en el primer parrafo y  es el primer numero que tiene cua$
+touch red.conf
+#falta el punto 3
+mkdir PRACTICA_3
+tar -cvf PRACTICA_3_VELASCO_SANTIAGO_ALBERT_JAVIER.tar PRACTICA_3
+gzip -9 PRACTICA_3_VELASCO_SANTIAGO_ALBERT_JAVIER.tar
